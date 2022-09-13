a65f43329821efaea649827742b1f0f84d536929
rpturbina <rizki.pratama.turbina@gmail.com>
2022-09-13 21:50:23 +0700

    implement theme switcher

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
+import ThemeContext from './context/ThemeContext';
 import AddNewPage from './pages/AddNewPage';
 import ArchivePage from './pages/ArchivePage';
 import DetailPage from './pages/DetailPage';
@@ -14,6 +15,28 @@ import { getUserLogged, putAccessToken } from './utils/network-data';
 function App() {
   const [authedUser, setAuthedUser] = React.useState(null);
   const [initializing, setInitializing] = React.useState(true);
+  const [theme, setTheme] = React.useState(() => {
+    return localStorage.getItem('theme') || 'light';
+  });
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === 'light' ? 'dark' : 'light';
+      localStorage.setItem('theme', newTheme);
+      return newTheme;
+    });
+  };
+
+  const themeContextValue = React.useMemo(() => {
+    return {
+      theme,
+      toggleTheme,
+    };
+  }, [theme]);
+
+  React.useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
 
   const onLoginSuccess = async ({ accessToken }) => {
     putAccessToken(accessToken);
@@ -38,25 +61,27 @@ function App() {
   }, []);
 
   return initializing ? null : (
-    <div className='app-container'>
-      <Header logout={onLogout} isAuthed={authedUser !== null} />
-      <main>
-        {authedUser ? (
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/notes/:id' element={<DetailPage />} />
-            <Route path='/notes/new' element={<AddNewPage />} />
-            <Route path='/archives' element={<ArchivePage />} />
-            <Route path='*' element={<NoMatchPage />} />
-          </Routes>
-        ) : (
-          <Routes>
-            <Route path='/*' element={<LoginPage loginSuccess={onLoginSuccess} />} />
-            <Route path='/register' element={<RegisterPage />} />
-          </Routes>
-        )}
-      </main>
-    </div>
+    <ThemeContext.Provider value={themeContextValue}>
+      <div className='app-container'>
+        <Header logout={onLogout} isAuthed={authedUser !== null} />
+        <main>
+          {authedUser ? (
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/notes/:id' element={<DetailPage />} />
+              <Route path='/notes/new' element={<AddNewPage />} />
+              <Route path='/archives' element={<ArchivePage />} />
+              <Route path='*' element={<NoMatchPage />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path='/*' element={<LoginPage loginSuccess={onLoginSuccess} />} />
+              <Route path='/register' element={<RegisterPage />} />
+            </Routes>
+          )}
+        </main>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { BsMoon as MoonIcon, BsSun as SunIcon } from 'react-icons/bs';
 import { IoIosLogOut } from 'react-icons/io';
 import { Link } from 'react-router-dom';
 
+import ThemeContext from '../context/ThemeContext';
 import Navigation from './Navigation';
 
 const Header = ({ logout, isAuthed }) => {
+  const { theme, toggleTheme } = React.useContext(ThemeContext);
   return (
     <header>
       <h1>
         <Link to='/'>notes.self</Link>
       </h1>
+      <button className='toggle-theme' onClick={toggleTheme}>
+        {theme === 'light' ? <MoonIcon /> : <SunIcon />}
+      </button>
       {isAuthed && (
         <>
           <Navigation />
